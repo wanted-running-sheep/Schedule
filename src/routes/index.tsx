74930b005cd66e678d6c81b8309d57046c5686fb
CHOI <tlacjd665@gmail.com>
2022-07-28 13:46:47 +0900

    ✨ feat:  기본적인 라우팅 추가

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,15 @@
+import AddSchedule from '@/pages/AddSchedule';
+import Scedule from '@/pages/Scedule';
 import React from 'react';
-import { Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 const Router = () => {
-  return <Routes></Routes>;
+  return (
+    <Routes>
+      <Route path="/" element={<Scedule />} />
+      <Route path="/add" element={<AddSchedule />} />
+    </Routes>
+  );
 };
 
 export default Router;
