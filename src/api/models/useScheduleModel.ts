05d50c753e259eafaeca1e95165a989ffa795a6c
jungjaechoi <joong8812@gmail.com>
2022-07-29 17:13:34 +0900

    ✨ feat: 중복 일정 추가 방지 구현

--- src/api/models/useScheduleModel.ts
+++ src/api/models/useScheduleModel.ts
@@ -20,6 +20,24 @@ const useScheduleModel = () => {
     }
   };
 
+  const checkSavedScheduleData = async (day: string, startTime: number) => {
+    try {
+      const scheduleResponse: AxiosResponse<ScheduleInterface[]> =
+        await apiRequest.get<ScheduleInterface[]>(
+          `${ApiUrlEnum.SCHEDULE}?day=${day}&startTime=${startTime}`
+        );
+      const data = scheduleResponse.data;
+      const msg = data.length > 0 ? '중복 일정이 있습니다.' : '';
+      const result = data.length > 0 ? false : true;
+      return { msg: msg, result: result, data: data };
+    } catch (error) {
+      return {
+        msg: '오류가 발생하였습니다. 관리자에게 문의 하세요.',
+        result: false,
+      };
+    }
+  };
+
   const createtSchedule = async <T>(data: T) => {
     try {
       await apiRequest.post<T>(ApiUrlEnum.SCHEDULE, data);
@@ -41,6 +59,7 @@ const useScheduleModel = () => {
     getScheduleData,
     createtSchedule,
     deleteSchedule,
+    checkSavedScheduleData,
   };
 };
 
